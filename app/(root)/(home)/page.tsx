be9b4160d5a0c09df186544269c26c35d0da82ee
fawasam <fawasam5@gmail.com>
2024-05-20 22:03:16 +0530

    auth setup

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      {/* create a login form */}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
